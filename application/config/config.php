675d36a09c14eaa7e6c0e52ff24923fb6535866f
Ialimijoro <mijororakotoniaina@gmail.com>
2016-05-06 11:51:29 +0300

    correction url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/gestion-notes-medecine/';
+$config['base_url'] = 'http://localhost:8888/projet-medecine/';
 
 /*
 |--------------------------------------------------------------------------
