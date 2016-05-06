70ee31507e2add60831187a95e30b8c66830b529
Ialimijoro <mijororakotoniaina@gmail.com>
2016-05-06 14:29:01 +0300

    changement viewport

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> <?php echo $title; ?> </title>
 	<link href=" <?php echo base_url('public/css/bootstrap.css'); ?> " rel="stylesheet" type="text/css" />
 	<link href=" <?php echo base_url('public/css/style.css'); ?> " rel="stylesheet" type="text/css" />
